3147eb5b839ddf95eb196c2eb07f30598cae6693
Jonathan Smith <jonathanstrongsmith@gmail.com>
2026-05-12 10:53:09 -0700

    Add the main index file to link the other files for EPDG Landing Page

---
+++ Emerson_Empire/src/Components/EPDG/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import WhoWeHelp from "./WhoWeHelp";
+import BusinessSupport from "./BusinessSupport";
+import Reviews from "./Reviews";
+import About from "./About";
+import FinancialEducation from "./FinancialEducation";
+import AgencyFAQ from "./AgencyFAQ";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+const Agency: FC = () => {
+  return (
+    <div className="font-sans text-gray-800 bg-white">
+      <Navbar />
+      <Hero />
+      <WhoWeHelp />
+      <BusinessSupport />
+      <Reviews />
+      <About />
+      <FinancialEducation />
+      <AgencyFAQ />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Agency;
